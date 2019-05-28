e0d38b0fb07bae0db9ec56ee8f6a4a5a8ea4920e
Adam Gilmore <adam.gilmore@outlook.com>
2019-05-28 21:14:29 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+
+	log.Fatal(http.ListenAndServe(":8081", router))
+}
+
+// Index handles root http requests
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
